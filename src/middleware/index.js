fca5fab5b6225e8bc6a4e18f575d394d64815670
Michael Mullany <mamullany@gmail.com>
2022-05-12 15:25:49 +0100

    Add email middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -12,11 +12,39 @@ exports.hashPass = async (req, res, next) => {
 
 exports.comparePass = async (req, res) => {
     try {
-        const compRes = await bcrypt.compare(req.body.password, req.body.db.password);
-        if (compRes) {
+        const match = await bcrypt.compare(req.body.password, req.body.db.password);
+        if (match) {
             res.status(200).send("User successfully signed in");
         } else {
             res.status(401).send("Bad credentials");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: error.message });
+    }
+}
+
+exports.checkEmailValid = async (req, res, next) => {
+    try {
+        const valid = await req.body.email.match(/.+\@.+\..+/);
+        if (valid) {
+            next();
+        } else {
+            res.status(400).send("Invalid Email Address");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: error.message });
+    }
+}
+
+exports.checkEmailValidUpdate = async (req, res, next) => {
+    try {
+        const valid = req.body.update.email.match(/.+\@.+\..+/);
+        if (valid) {
+            next();
+        } else {
+            res.status(400).send("New Email Address is Invalid");
         }
     } catch (error) {
         console.log(error);
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,14 +1,14 @@
 const { Router } = require("express");
 const { addUser, getUser, listUsers, updateUser, deleteUser } = require("./userControllers");
-const { hashPass, comparePass } = require("../middleware");
+const { checkEmailValid, checkEmailValidUpdate, hashPass, comparePass } = require("../middleware");
 
 const userRouter = Router();
 
 // use http verb post to add data to our movie endpoint
-userRouter.post("/user", hashPass, addUser);
+userRouter.post("/user", checkEmailValid, hashPass, addUser);
 userRouter.get("/user/login", getUser, comparePass);
 userRouter.get("/user", listUsers);
-userRouter.put("/user", updateUser);
+userRouter.put("/user", checkEmailValidUpdate, updateUser);
 userRouter.delete("/user", deleteUser);
 
 module.exports = userRouter;
