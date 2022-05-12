a2fa676c9845d181c48ca0bbc18db055803402b5
Michael Mullany <mamullany@gmail.com>
2022-05-12 14:26:17 +0100

    Add CRUD operations for User

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -20,3 +20,33 @@ exports.getUser = async (req, res, next) => {
         res.status(500).send({ error: error.message });
     }
 }
+
+exports.listUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).send({ users });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: error.message })
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    try {
+        const confirmation = await User.updateOne(req.body.filter, req.body.update)
+        res.status(200).send({ confirmation });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: error.message })
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const confirmation = await User.deleteOne(req.body)
+        res.status(200).send({ confirmation })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: error.message })
+    }
+}
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { addUser, getUser } = require("./userControllers");
+const { addUser, getUser, listUsers, updateUser, deleteUser } = require("./userControllers");
 const { hashPass, comparePass } = require("../middleware");
 
 const userRouter = Router();
@@ -7,5 +7,8 @@ const userRouter = Router();
 // use http verb post to add data to our movie endpoint
 userRouter.post("/user", hashPass, addUser);
 userRouter.get("/user/login", getUser, comparePass);
+userRouter.get("/user", listUsers);
+userRouter.put("/user", updateUser);
+userRouter.delete("/user", deleteUser);
 
 module.exports = userRouter;
